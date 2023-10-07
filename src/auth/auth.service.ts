591e0b1cf8b57fb07540c09fad7d4c668bb985a9
GeovannyTS <yowtecum@gmail.com>
2023-10-07 15:57:03 -0600

    save user and encrypt password
    Implements libry bcryptjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from "bcryptjs";
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
+import { User } from './entities/user.entity';
+
+
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
+
+  constructor(
+    @InjectModel(User.name) 
+    private userModel: Model<User>,
+  ){}
+
+
+  async create(createUserDto: CreateUserDto):Promise<User> {
     console.log(createUserDto)
-    return 'This action adds a new auth';
+
+    // crear usuario. Funciona pero en bruto
+    // const newUser = new this.userModel(createUserDto);
+    // return newUser.save();
+
+    try {
+      
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        // 1 encriptar la contraseña con desestructuracion
+        password: bcryptjs.hashSync(password, 10),
+        ...userData
+      });
+
+      // 1 Guardar el usuario
+      await newUser.save();
+      // mostrar toda la info sin mostrar el password por que no es necesario
+      const { password:_, ...user } = newUser.toJSON();
+
+
+      return user;
+
+      // 1 Generar el JWT
+
+    } catch (error) {
+      if (error.code === 11000){
+        throw new BadRequestException(`${ createUserDto.email} ya existe `)
+      }
+
+      throw new InternalServerErrorException('No se logro insertar el nuevo registro')
+    }
+
+
   }
 
   findAll() {
