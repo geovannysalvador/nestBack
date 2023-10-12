78a4dffc4bcbb4c3ce0c0f37d65870447d26cd50
GeovannyTS <yowtecum@gmail.com>
2023-10-11 19:43:06 -0600

    Generate new JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,12 @@
+
 import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 import { CreateUserDto, UpdateAuthDto, LoginDto, RegisterUserDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
 // import { UpdateAuthDto } from './dto/update-auth.dto';
 // import { LoginDto } from './dto/login.dto';
 // import { RegisterDto } from './dto/register.dto';
@@ -29,23 +33,36 @@ export class AuthController {
   @UseGuards(AuthGuard)
   @Get()
   findAll( @Request() req:Request ) {
-    const user = req['user']
+    // const user = req['user']
     // return user;
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  // Loginresponse
+  @UseGuards(AuthGuard)
+  @Get('/check-token')
+  checkToken( @Request() req:Request ):LoginResponse {
+
+    const user = req['user'] as User;
+    return {
+      user,
+      token: this.authService.getJwToken({ id:user._id })
+    }
+
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
 }
